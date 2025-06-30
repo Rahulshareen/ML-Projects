5e24fcf12f9490188fb70f967cd8090b9fd08c8f
Rahul Shareen <rahulshareen@gmail.com>
2025-06-30 12:51:07 +0530

    Reinstalled venv

--- source/exception.py
+++ source/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from source.logger import logging
 
 def error_message_detail(error,error_details:sys):
     _,_,exc_tb = error_details.exc_info()
@@ -14,4 +14,4 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail) 
     def __str__(self):
         return self.error_message
-    
+
